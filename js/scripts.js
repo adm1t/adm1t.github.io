e403060d0d5cfdec679090da3ed332634785c507
adm1t <filippov95@mail.ru>
2019-09-17 18:42:49 +0300

    style fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-var  tl = new TimelineMax();
+document.addEventListener('DOMContentLoaded', function(){
 
-var mainWrapper = document.querySelector('.main-wrapper');
-var title = document.querySelectorAll(".title");
-var letters = document.querySelectorAll(".title__letter");
-var delimeter = document.querySelectorAll(".title__delimeter");
-var contactsSection = document.querySelector(".contacts");
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
 
-window.addEventListener("load", function(){
+    window.addEventListener('resize', function(){
+        vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', vh + 'px');
+    });
 
+    var title = document.querySelectorAll(".title");
+    var letters = document.querySelectorAll(".title__letter");
+    var delimeter = document.querySelectorAll(".title__delimeter");
+    var contactsSection = document.querySelector(".contacts");
+
+    var tl = new TimelineMax();
     tl
         .to(title, 0, {y: 75})
         .staggerFromTo(letters, 0.5, {backgroundColor:"black"}, {opacity:1, backgroundColor:"unset",  ease: Expo. easeOut}, 0.5)
